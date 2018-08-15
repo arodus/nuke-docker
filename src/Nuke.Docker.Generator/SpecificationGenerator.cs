a357311bc77c70be3997c3f39ef5a5d4a2806636
Sebastian Karasek <sebastian@karasek.io>
2018-08-15 23:34:53 +0200

    Fix format of generated specification file

--- src/Nuke.Docker.Generator/SpecificationGenerator.cs
+++ src/Nuke.Docker.Generator/SpecificationGenerator.cs
@@ -3,8 +3,10 @@
 // https://github.com/nuke-build/docker/blob/master/LICENSE
 
 using System;
+using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
+using Nuke.CodeGeneration;
 using Nuke.Common.IO;
 
 namespace Nuke.Docker.Generator
@@ -13,20 +15,15 @@ namespace Nuke.Docker.Generator
     {
         public static void GenerateSpecifications(SpecificationGeneratorSettings settings)
         {
-            Console.WriteLine($"Generating docker specifications...");
+            Console.WriteLine("Generating docker specifications...");
             var definitions =
                 DefinitionFetcher.GetCommandDefinitionsFromFolder(settings.DefinitonFolder, settings.Reference, settings.CommandsToSkip);
             var tool = DefinitionParser.GenerateTool(definitions);
 
-            var specification = JsonConvert.SerializeObject(tool,
-                new JsonSerializerSettings
-                {
-                    NullValueHandling = NullValueHandling.Ignore,
-                    Formatting = Formatting.Indented,
-                    DefaultValueHandling = DefaultValueHandling.Include
-                });
-
-            TextTasks.WriteAllText(PathConstruction.Combine(settings.OutputFolder, "Docker.json"), specification);
+            Directory.CreateDirectory(settings.OutputFolder);
+            tool.DefinitionFile = PathConstruction.Combine(settings.OutputFolder, "Docker.json");
+            ToolSerializer.Save(tool);
+           
             Console.WriteLine();
             Console.WriteLine("Generation finished.");
             Console.WriteLine($"Created Tasks: {tool.Tasks.Count}");
